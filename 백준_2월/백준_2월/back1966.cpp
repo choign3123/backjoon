ededccc547f64b8851d5164a0473a43a6dba1367
choign3123 <choign3123@naver.com>
2022-03-01 00:16:07 +0900

    백준 1966 큐 프린터
    -시뮬레이션
    -예전에 이문제 봤을 땐 풀이법이 안떠올랐는데.. 지금이 금방 풀이가 떠올라서 놀랐다

--- 백준_2월/백준_2월/back1966.cpp
+++ 백준_2월/백준_2월/back1966.cpp
@@ -1,22 +1,58 @@
 #include <cstdio>
 #include <algorithm>
-#include <stack>
+#include <list>
 
 using namespace std;
 
+list<pair<int, bool>>::iterator itr;
+int n_testcase;
+int n_doc, key_idx;
+
 int main(void)
 {
-	char c;
-	stack<char> stk;
+	scanf_s("%d", &n_testcase);
+	 
+	for (int k = 0; k < n_testcase; k++) { //테스트 케이스의 개수만큼.
+		scanf_s("%d %d", &n_doc, &key_idx);
 
-	while (1) {
-		scanf_s("%c", &c, sizeof(c));
-		stk.push(c);
-	}
+		list<pair<int, bool>> lst;
+		int n;
+		for (int i = 0; i < n_doc; i++) {
+			scanf_s("%d", &n);
+			if (i == key_idx) {
+				lst.push_back({ n, true });
+			}
+			else {
+				lst.push_back({ n, false });
+			}
+		}
 
-	int anw = stk.
+		pair<int, bool> doc;
+		bool isPrint; //출력 될지 안될지.
+		int cnt = 0; //출력된 문서의 개수
+		while (!lst.empty()) { //프린터에 남은 문서가 없을 때까지.
+			doc = lst.front(); //첫번째 문서.
+			lst.pop_front();
+			isPrint = true;
 
-	for (int i = 0; i < stk.size(); i++) {
+			itr = lst.begin(); //실상 두번째 문서부터.
+			while (itr != lst.end()) {
+				if (doc.first < itr->first) { //뒤에 더 중요한 문서가 있다면.
+					lst.push_back(doc); //뒤로 밂.
+					isPrint = false; //출력 안됨. 뒤로 밀림.
+					break;
+				}
+				itr++; //다음 문서 확인.
+			}
+
+			if (isPrint) { //출력 가능하면
+				cnt++;
+				if (doc.second == true) { //key_idx의 문서였다면.
+					printf("%d\n", cnt);
+					break;
+				}
+			}
+		}
 
 	}
 }
