1bccacf81924a5d83cf6dbe5c5c48fae58c9a04b
choign3123 <choign3123@naver.com>
2023-02-12 22:13:06 +0900

    백준 10828 스택 풂

--- 백준_2023_1월/backjoon/backjoon/stack10828.cpp
+++ 백준_2023_1월/backjoon/backjoon/stack10828.cpp
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <stack>
+
+using namespace std;
+
+stack<int> st;
 
 int main(void) {
 
@@ -11,10 +16,32 @@ int main(void) {
 
 	int arg;
 	for (int i = 0; i < n; i++) {
-		scanf("%s", comd);
+		scanf("%s", comd);//명령 입력받기
+
 		if (strcmp(comd, "push") == 0) {
 			scanf("%d", &arg);
-			printf("%d\n", arg);
+			st.push(arg);
+		}
+		else if (strcmp(comd, "pop") == 0) {
+			if (st.empty()) {
+				printf("-1\n");
+				continue;
+			}
+			printf("%d\n", st.top());
+			st.pop();
+		}
+		else if (strcmp(comd, "size") == 0) {
+			printf("%d\n", st.size());
+		}
+		else if (strcmp(comd, "empty") == 0) {
+			printf("%d\n", st.empty());
+		}
+		else {
+			if (st.empty()) {
+				printf("-1\n");
+				continue;
+			}
+			printf("%d\n", st.top());
 		}
 	}
 }
